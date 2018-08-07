c6615e153d811baaef3477e15d163178b85f8590
Ro6afF <dimo_chanev@windowslive.com>
2018-08-07 17:57:06 +0300

    backup

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod analytics;
 mod midi;
 use midi::*;
 use rand::prelude::*;
-use std::fs;
+mod neural_network;
 
 fn main() {
-    let inp = MidiFile::read_file("/home/ro6aff/compmus/examples/bach_tocatta_fugue_d_minor.mid".to_string());
+    let inp = MidiFile::read_file(
+        "/home/ro6aff/compmus/examples/mozart_symphony_no-40.mid".to_string(),
+    );
     println!("{}\n---------------------------", inp);
     let plok = analytics::construct_probalility_table(&inp);
     for (k, v) in &plok {
@@ -23,8 +25,14 @@ fn main() {
     let mut chunks = vec![];
     chunks.push(Chunk::Header(HeaderChunk::new(1, 1, 16)));
     let mut events = vec![];
-    events.push(Event::Meta(MetaEvent::new(0, 0x51, 03, vec![0xf, 0x42, 0x40])));
+    events.push(Event::Meta(MetaEvent::new(
+        0,
+        0x51,
+        03,
+        vec![0xf, 0x42, 0x40],
+    )));
     events.push(Event::Meta(MetaEvent::new(0, 0x58, 04, vec![4, 2, 48, 8])));
+    events.push(Event::Midi(MidiEvent::new(0, 0b11000000, 0, 20, 0)));
     for _ in 0..100 {
         println!("{}", NOTES[currn as usize]);
         events.push(Event::Midi(MidiEvent::new(0, 0b10010000, 0, currn, 127)));
@@ -35,8 +43,7 @@ fn main() {
             match i {
                 (a, b) => {
                     currc += b;
-                    println!("{} <= {}", currc, blqh);
-                    if  blqh <= currc {
+                    if blqh <= currc {
                         currn = *a;
                         break;
                     }
@@ -46,6 +53,9 @@ fn main() {
     }
     events.push(Event::Meta(MetaEvent::new(0, 0x2f, 00, vec![])));
     chunks.push(Chunk::Track(TrackChunk::from(events)));
-    let res = MidiFile::new("/home/ro6aff/compmus/examples/result_2.mid".to_string(), chunks);
+    let res = MidiFile::new(
+        "/home/ro6aff/compmus/examples/result_2.mid".to_string(),
+        chunks,
+    );
     res.write_file();
 }
