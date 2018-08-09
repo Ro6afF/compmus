4a6133999373faaf911a130e39120e6efb508fae
Ro6afF <dimo_chanev@windowslive.com>
2018-08-09 11:33:20 +0300

    made pair probability analitics

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,22 @@ use midi::*;
 use rand::prelude::*;
 
 fn main() {
-    let inp = MidiFile::read_file("/home/ro6aff/compmus/examples/bach_tocatta_fugue_d_minor.mid".to_string());
+    let inp =
+        MidiFile::read_file("/home/ro6aff/compmus/examples/chopin_nocturne_9_2.mid".to_string());
     println!("{}\n---------------------------", inp);
     let plok = analytics::construct_probalility_table(&inp);
-    let mut currn = (61, 512);
-    for (k, v) in &plok {
-        match k {
-            (x, z) => {
-                println!("({}, {}):", NOTES[*x as usize], z);
-                //currn = (*x, *z)
-            }
-        }
-        for i in v {
-            match i {
-                ((a, b), c) => println!("\t({}, {}) -> {}", NOTES[*a as usize], b, c),
-            }
+    let mut currn = (52, 55);
+    println!("{:?}", plok);
+    for ((a, b), v) in &plok {
+        println!("{} ; {} ->", NOTES[*a as usize], NOTES[*b as usize]);
+        for (x, y) in v {
+            println!("\t{} -> {}", NOTES[*x as usize], y)
         }
     }
     let mut rng = thread_rng();
     println!("----------------------------");
     let mut chunks = vec![];
-    chunks.push(Chunk::Header(HeaderChunk::new(1, 1, 256)));
+    chunks.push(Chunk::Header(HeaderChunk::new(1, 1, 16)));
     let mut events = vec![];
     events.push(Event::Meta(MetaEvent::new(
         0,
@@ -36,24 +31,41 @@ fn main() {
     events.push(Event::Meta(MetaEvent::new(0, 0x58, 04, vec![4, 2, 48, 8])));
     for _ in 0..100 {
         match currn {
-            (a, b) => {
-                println!("{}, {}", NOTES[a as usize], b);
+            (a, _) => {
                 events.push(Event::Midi(MidiEvent::new(0, 0b10010000, 0, a, 127)));
-                events.push(Event::Midi(MidiEvent::new(b, 0b10000000, 0, a, 0)));
+                events.push(Event::Midi(MidiEvent::new(8, 0b10000000, 0, a, 0)));
             }
         }
         let mut blqh: f32 = rng.gen();
         let mut currc = 0.0;
-        for i in &plok[&currn] {
-            match i {
-                (a, b) => {
-                    currc += b;
-                    if blqh <= currc {
-                        currn = *a;
-                        break;
+        println!("{:?}", currn);
+        if (&plok).contains_key(&currn) {
+            for i in &plok[&currn] {
+                match i {
+                    (a, b) => {
+                        currc += b;
+                        if blqh <= currc {
+                            let ln;
+                            match currn {
+                                (_, w) => ln = w,
+                            }
+                            currn = (ln, *a);
+                            break;
+                        }
                     }
                 }
             }
+        } else {
+            let mut currn1 = (0, 0);
+            for ((a, _), _) in &plok {
+                match &currn {
+                    (x, y) => if a == x {
+                        currn1 = (*x, *y);
+                    },
+                    _ => panic!(),
+                }
+            }
+            currn = currn1;
         }
     }
     events.push(Event::Meta(MetaEvent::new(0, 0x2f, 00, vec![])));
